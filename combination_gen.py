39e16f2647a919eccf773db8ea81e7b8c47d93ac
Phasit RUANGMAK <phasit2005@gmail.com>
2023-11-09 10:06:26 +0700

    Created the new file combination_gen.py and the function inside.

---
+++ combination_gen.py
@@ -0,0 +1,47 @@
+import itertools
+
+
+def gen_comb_list(list_set):
+    '''
+        Parameters:
+            list_set: a list of lists where each contains at least one element
+
+        Returns:
+            a list of lists, each of which is made from a combination of elements in each list in list_set
+
+        Examples:
+            gen_comb_list([[1, 2, 3]]) returns [[1], [2], [3]]
+            gen_comb_list([[1, 2, 3], [4, 5]]) returns [[1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5]]
+            gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
+    '''
+    if len(list_set) == 1:
+        result = []
+        for i in list_set:
+            for item in i:
+                inner_ls = []
+                inner_ls.append(item)
+                result.append(inner_ls)
+    else:
+        result = []
+        # store the first list in the given list in a variable
+        first_list = list_set[0]
+        # for loop that runs for item in first_list
+        for i in first_list:
+            # store the remaining lists in a new variable
+            remaining_list = gen_comb_list(list_set[1:])
+            # for item in the remaining list
+            for combine in remaining_list:
+                # add the member from the extracted list to the i of the first list
+                result.append([i] + combine)
+    return result
+
+
+
+
+
+print(gen_comb_list([[1, 2, 3]]))
+print()
+print(gen_comb_list([[1, 2, 3], [4, 5]]))
+print()
+print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
+
